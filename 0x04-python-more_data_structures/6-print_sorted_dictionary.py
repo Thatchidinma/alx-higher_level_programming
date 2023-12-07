118f79374c21b8987f14dfe51690a0abc82eae40
Thatchidinma <ndukubachidinma@gmail.com>
2023-12-07 16:20:32 +0000

    sorted dict

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_dict = {}
     for key, value in sorted(a_dictionary.items()):
-        sorted_dict[key] = value
         print("{}: {}".format(key, value))
