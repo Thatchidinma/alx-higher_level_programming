edc4e952130f53d6027ca442fb284868cf97fdfb
Thatchidinma <ndukubachidinma@gmail.com>
2024-03-10 15:24:50 +0000

    try/except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     counter = 0
-    for element in my_list:
+    for element in range(x):
         try:
             print("{}".format(my_list[element]), end="")
             counter += 1
